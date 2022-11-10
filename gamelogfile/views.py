ea9499d369cc5a58d886d9c7dc04d334753ec214
ks4n <mwkresolve@gmail.com>
2022-11-09 22:38:59 -0300

    aviso edição de log ja existente

--- gamelogfile/views.py
+++ gamelogfile/views.py
@@ -8,18 +8,21 @@ from controller.functionsdb import *
 class LogPageView(TemplateView):
     template_name = "log.html"
 
+
     def get(self, request, *args):
         my_log = User.objects.filter(username=request.user).values('log')[0]['log']
         return render(request, self.template_name, {'mylog': my_log})
 
     def post(self, request):
+        my_log = User.objects.filter(username=request.user).values('log')[0]['log']
         if request.method == "POST":
             if request.POST["editlog"]:
                 editlogactive = len(Processes.objects.filter(userid=request.user, action=1,completed=False))
                 # usuario só pode ter 1 task ativa para completar
                 if editlogactive > 0:
                     # criar msg de aviso no front que ja existe uma tarefa em andamento
-                    return HttpResponseRedirect("/log/")
+                    msg_erro = 'ja existe uma tarefa igual em execução'
+                    return render(request, self.template_name, {'mylog': my_log, 'msg_erro':msg_erro})
                 else:
                     endtime = datetime.now() + timedelta(seconds=3)
                     current_log = request.POST.get('logarea')
