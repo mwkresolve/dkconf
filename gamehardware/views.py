a81fc2fcb76cc368fda89f70eb5d5e9a361b6466
ks4n <mwkresolve@gmail.com>
2022-11-17 19:59:38 -0300

    ajuste erro logout bank

--- gamehardware/views.py
+++ gamehardware/views.py
@@ -5,6 +5,8 @@ from controller.models import Hardware
 class HardwareView(TemplateView):
     template_name = "basehardware.html"
 
-    def get(self, request):
+    def get(self, request, *args):
         user_hardware = Hardware.objects.filter(userid=request.user).values()
-        return render(request, self.template_name, {'user_hardware':user_hardware})
+        return render(request, self.template_name, {'user_hardware': user_hardware})
+    def post(self, request, *args):
+        pass
